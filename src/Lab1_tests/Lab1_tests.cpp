3d867a19106e6a8d81c0efd4146ac19d4693eb0d
Andrii <andpris527@gmail.com>
2023-09-27 18:03:49 +0300

    Add infrastucture

---
+++ src/Lab1_tests/Lab1_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab1tests
+{
+	TEST_CLASS(Lab1tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
