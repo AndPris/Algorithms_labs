83326178ae70b57430c92ba40696acf2f58b0d2a
Andrii <andpris527@gmail.com>
2023-09-27 19:02:55 +0300

    Add constructor and destructor to FileHandler class

--- src/Lab1/FileHandler.cpp
+++ src/Lab1/FileHandler.cpp
@@ -6,3 +6,20 @@ int FileHandler::generate_number_from_range(int low_border, int top_border) {
 
 	return low_border + rand() % (top_border - low_border + 1);
 }
+
+
+FileHandler::FileHandler(int amount_of_supporting_files) : amount_of_suporting_files(amount_of_suporting_files) {
+	file_extension = ".bin";
+	initial_file_name = "initial" + file_extension;
+	supporting_file_prefix = "supporting_";
+
+	supporting_files_names = new string[amount_of_suporting_files];
+	for (int i = 0; i < amount_of_suporting_files; i++)
+		supporting_files_names[i] = supporting_file_prefix + to_string(i) + file_extension;
+
+
+}
+
+FileHandler::~FileHandler() {
+	delete[] supporting_files_names;
+}
--- src/Lab1/FileHandler.h
+++ src/Lab1/FileHandler.h
@@ -7,6 +7,17 @@ using namespace std;
 
 class FileHandler
 {
+	const int amount_of_suporting_files;
+	
+	string file_extension;
+	string initial_file_name;
+	string supporting_file_prefix;
+	string *supporting_files_names;
+
 	int generate_number_from_range(int low_border, int top_border);
+public:
+	FileHandler(int amount_of_supporting_files);
+
+	~FileHandler();
 };
 
