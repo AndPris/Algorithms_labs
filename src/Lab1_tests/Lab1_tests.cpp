1a947831cfd2aae7bf332476ae5206f62a3e0ea9
Andrii <andpris527@gmail.com>
2023-10-02 21:23:43 +0300

    Add test for file creation

--- src/Lab1_tests/Lab1_tests.cpp
+++ src/Lab1_tests/Lab1_tests.cpp
@@ -1,18 +1,35 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Lab1/FileSorter.h"
+#include "../Lab1/FileCreator.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Lab1tests
 {
-	TEST_CLASS(FileSorter)
+	TEST_CLASS(FileCreatorTest)
 	{
 	public:
 
-		TEST_METHOD(test)
+		TEST_METHOD(creatre_initial_file)
 		{
-			
+			string file_path = "creating_test.bin";
+			wstring file_path_w = wstring(file_path.begin(), file_path.end());
+			const wchar_t* result_path = file_path_w.c_str();
+			const int size_of_file_in_mb = 1;
+
+
+			FileCreator creator(result_path, size_of_file_in_mb);
+			creator.create_initial_file();
+
+			ifstream created_file(file_path, ios::binary);
+			Assert::IsTrue(created_file.is_open());
+
+			created_file.seekg(0, ios::end);
+			int file_size = created_file.tellg();
+			Assert::AreEqual(file_size, (size_of_file_in_mb * 1024 * 1024));
+			created_file.close();
+
+			remove(file_path.c_str());
 		}
 	};
 }
