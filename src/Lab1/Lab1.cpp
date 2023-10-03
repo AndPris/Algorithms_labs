cc2d1659bfa6fb5ad3a5db65da3748e6025b0797
Andrii <andpris527@gmail.com>
2023-10-03 09:59:29 +0300

    Add restarting of program

--- src/Lab1/Lab1.cpp
+++ src/Lab1/Lab1.cpp
@@ -10,7 +10,7 @@ int input_positive_number() {
     bool repeat;
     do {
         repeat = false;
-        if (!(cin >> number) || ((ch = getchar()) != '\n') || (number < 0)) {
+        if (!(cin >> number) || ((ch = getchar()) != '\n') || (number <= 0)) {
             cout << "Invalid data, input integer number greater 0, please." << endl;
             cin.clear();
             cin.ignore(100, '\n');
@@ -26,33 +26,39 @@ int main() {
     string supporting_file_prefix = "supporting_";
 
     const wchar_t* initial_file_path = L"initial.bin";
+    int repeat;
+    do {
+        cout << "Enter size of file to sort in megabytes:" << endl;
+        int size_of_initial_file_in_mb = input_positive_number();
 
-    cout << "Enter size of file to sort in megabytes:" << endl;
-    int size_of_initial_file_in_mb = input_positive_number();
+        FileCreator creator(initial_file_path, size_of_initial_file_in_mb);
 
-    FileCreator creator(initial_file_path, size_of_initial_file_in_mb);
-
-    clock_t beginning_of_creation, end_of_creation,
+        clock_t beginning_of_creation, end_of_creation,
             beginning_of_sorting, end_of_sorting;
 
-    beginning_of_creation = clock();
-    creator.create_initial_file();
-    
-    end_of_creation = clock();
-    cout << "Creation of the file took " << fixed << double(end_of_creation - beginning_of_creation) / CLOCKS_PER_SEC << setprecision(5) << " seconds" << endl;
-    try {
-        FileSorter sorter(creator.get_initial_file_path(), file_extension, supporting_file_prefix, AMOUNT_OF_SUPPORTING_FILES);
-        beginning_of_sorting = clock();
-       
-        sorter.polyphase_merge_sort();
+        beginning_of_creation = clock();
+        creator.create_initial_file();
 
-        end_of_sorting = clock();
+        end_of_creation = clock();
+        cout << "Creation of the file took " << fixed << double(end_of_creation - beginning_of_creation) / CLOCKS_PER_SEC << setprecision(5) << " seconds" << endl;
+        try {
+            FileSorter sorter(creator.get_initial_file_path(), file_extension, supporting_file_prefix, AMOUNT_OF_SUPPORTING_FILES);
+            beginning_of_sorting = clock();
 
-        cout << "Sorting of the file took " << fixed << double(end_of_sorting - beginning_of_sorting) / CLOCKS_PER_SEC << setprecision(5) << " seconds" << endl;
-    }
-    catch (const char* err) {
-        cout << err << endl;
-        return -1;
-    }
+            sorter.polyphase_merge_sort();
+
+            end_of_sorting = clock();
+
+            cout << "Sorting of the file took " << fixed << double(end_of_sorting - beginning_of_sorting) / CLOCKS_PER_SEC << setprecision(5) << " seconds" << endl;
+
+            cout << "Enter 1 to run program again or any other positive value to stop it:" << endl;
+            repeat = input_positive_number();
+        }
+        catch (const char* err) {
+            cout << err << endl;
+            return -1;
+        }
+    } while (repeat == 1);
+
     return 0;
 }
