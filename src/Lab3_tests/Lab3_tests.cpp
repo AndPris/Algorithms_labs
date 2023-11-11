3eb6070896103285dcd0b746d00af1d478210f45
Andrii <andpris527@gmail.com>
2023-11-11 20:01:58 +0200

    Add project for unit tests for lab 3

---
+++ src/Lab3_tests/Lab3_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab3/B_Tree.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab3tests
+{
+	TEST_CLASS(Lab3tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
