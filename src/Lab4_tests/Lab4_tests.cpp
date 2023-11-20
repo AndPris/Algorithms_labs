af462ab1886082bf12b4e14dba5da1dc80c5563b
Andrii <andpris527@gmail.com>
2023-11-20 14:44:34 +0200

    Add vertexes tests

--- src/Lab4_tests/Lab4_tests.cpp
+++ src/Lab4_tests/Lab4_tests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../Lab4/FullGraph.h"
+#include "../Lab4/Vertex_Edge_Path.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Lab4tests
@@ -13,6 +14,32 @@ namespace Lab4tests
 			int amountOfVertexes = 10;
 			FullGraph graph(amountOfVertexes);
 			Assert::AreEqual(amountOfVertexes, graph.getAmountOfVertexes());
+		}
+	};
+
+	TEST_CLASS(VertexTests)
+	{
+	public:
+
+		TEST_METHOD(getNumberTest) {
+			int number = 1;
+			Vertex vertex(number);
+			Assert::AreEqual(number, vertex.getNumber());
+		}
+
+		TEST_METHOD(equalVertexesComparrisonTest) {
+			int number = 1;
+			Vertex vertex1(number);
+			Vertex vertex2(number);
+			Assert::IsTrue(vertex1 == vertex2);
+		}
+
+		TEST_METHOD(nonEqualVertexesComparrisonTest) {
+			int number1 = 1;
+			int number2 = 2;
+			Vertex vertex1(number1);
+			Vertex vertex2(number2);
+			Assert::IsFalse(vertex1 == vertex2);
 		}
 	};
 }
