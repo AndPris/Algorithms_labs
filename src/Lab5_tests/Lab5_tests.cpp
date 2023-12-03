e58f9ea28a07f8e9c5222c6c330d37e214b51eaf
Andrii <andpris527@gmail.com>
2023-12-03 18:48:55 +0200

    Add infrastructure for lab5

---
+++ src/Lab5_tests/Lab5_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab5tests
+{
+	TEST_CLASS(Lab5tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
